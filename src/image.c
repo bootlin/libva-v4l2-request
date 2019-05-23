60233cbc906eef9ede5a93424bf1097c0f213411
Philipp Zabel <p.zabel@pengutronix.de>
2019-05-23 15:10:28 +0200

    HACK: disable tiled_to_planar conversion
    This works around a runtime dynamic linker error:
    
      $ vainfo
      libva info: VA-API version 1.1.0
      libva info: va_getDriverName() returns -1
      libva info: User requested driver 'v4l2_request'
      libva info: Trying to open /usr/lib/dri/v4l2_request_drv_video.so
      libva error: dlopen of /usr/lib/dri/v4l2_request_drv_video.so failed:
        /usr/lib/dri/v4l2_request_drv_video.so: undefined symbol: tiled_to_planar
      libva info: va_openDriver() returns -1
      vaInitialize failed with error code -1 (unknown libva error),exit

--- src/image.c
+++ src/image.c
@@ -155,13 +155,16 @@ static VAStatus copy_surface_to_image (struct request_data *driver_data,
 		return VA_STATUS_ERROR_INVALID_BUFFER;
 
 	for (i = 0; i < surface_object->destination_planes_count; i++) {
+#if 0
 		if (!video_format_is_linear(driver_data->video_format))
 			tiled_to_planar(surface_object->destination_data[i],
 					buffer_object->data + image->offsets[i],
 					image->pitches[i], image->width,
 					i == 0 ? image->height :
 						 image->height / 2);
-		else {
+		else
+#endif
+		{
 			memcpy(buffer_object->data + image->offsets[i],
 			       surface_object->destination_data[i],
 			       surface_object->destination_sizes[i]);
