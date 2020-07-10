0dcd84488fe5fe011a6ea2bf2d8c5b8e5ce22e23
Nicolas Chauvet <kwizart@gmail.com>
2020-07-10 14:10:02 +0200

    Fix condition to avoid arm specific code for Sunxi tiled format
    Signed-off-by: Nicolas Chauvet <kwizart@gmail.com>

--- src/image.c
+++ src/image.c
@@ -33,7 +33,9 @@
 #include <assert.h>
 #include <string.h>
 
+#ifdef __arm__
 #include "tiled_yuv.h"
+#endif
 #include "utils.h"
 #include "v4l2.h"
 
@@ -155,6 +157,7 @@ static VAStatus copy_surface_to_image (struct request_data *driver_data,
 		return VA_STATUS_ERROR_INVALID_BUFFER;
 
 	for (i = 0; i < surface_object->destination_planes_count; i++) {
+#ifdef __arm__
 		if (!video_format_is_linear(driver_data->video_format))
 			tiled_to_planar(surface_object->destination_data[i],
 					buffer_object->data + image->offsets[i],
@@ -162,10 +165,13 @@ static VAStatus copy_surface_to_image (struct request_data *driver_data,
 					i == 0 ? image->height :
 						 image->height / 2);
 		else {
+#endif
 			memcpy(buffer_object->data + image->offsets[i],
 			       surface_object->destination_data[i],
 			       surface_object->destination_sizes[i]);
+#ifdef __arm__
 		}
+#endif
 	}
 
 	return VA_STATUS_SUCCESS;
